b7e6d424f11655b5271f65ecf68b5ef99f5e2719
Tehraan khan <20co49@aitdgoa.edu.in>
2023-08-07 23:25:24 +0530

    update functionality is created

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,9 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
+import "./C.css"
 
 function Notes(props) {
+    const [showupdate, setshowupadte] = useState(false);
 
 
+    function UpdateForm() {
+        return (
+            <>
+                <div className="wrappaer"></div>
+                <div className="container2">
+                    <h1>Update The Note</h1>
+                    <input type="text" className="Ipt1"></input>
+                    <input type="text" className="Ipt2"></input>
+                    <button onClick={() => setshowupadte(false)}>Enter</button>
 
+                </div>
+
+            </>
+
+
+        );
+    }
 
     return (
 
@@ -19,10 +37,12 @@ function Notes(props) {
                 </div>
 
                 <button onClick={() => props.onDelete(props.id)} className="Delete-btn">Delete</button>
+                <button onClick={() => setshowupadte(true)} className="Update-btn">Update</button>
+
 
 
             </div>
-
+            {showupdate && <UpdateForm />}
 
 
 
